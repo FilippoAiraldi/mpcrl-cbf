a8af872c092ae6d9010a937019dfb30155a4a87f
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-10-18 16:29:10 +0200

    implementing the LTI env and some simpler controllers

---
+++ lti_example/env.py
@@ -0,0 +1,114 @@
+from typing import Any, TypeAlias, TypeVar
+
+import casadi as cs
+import gymnasium as gym
+import numpy as np
+import numpy.typing as npt
+from mpcrl.util.geometry import ConvexPolytopeUniformSampler
+
+ObsType: TypeAlias = npt.NDArray[np.floating]
+ActType: TypeAlias = npt.NDArray[np.floating]
+SymType = TypeVar("SymType", cs.SX, cs.MX)
+
+
+class ConstrainedLtiEnv(gym.Env[ObsType, ActType]):
+    """
+    ## Description
+
+    This environment simulates a simple discrete-time constrained LTI system.
+
+    ## Observation Space
+
+    The observation (a.k.a., state) space is an array of shape `(ns,)`, where `ns = 2`.
+    States are softly constrained to be within the interval `[-3, 3]`. If that's not the
+    case, a penalty is incurred based on `constraint_penalty`.
+
+    ## Action Space
+
+    The action is an array of shape `(na,)`, where `na = 2`, bounded to `[-0.5, 0.5]`.
+
+    ## Rewards/Costs
+
+    The reward here is intended as an linear quadratic regulation (LQR) cost, thus it
+    must be minized, with the goal being to regulate the system to the origin. The cost
+    is computed according to the current state and action (based on cost matrices `Q`
+    and `R`), but it also includes a penalisation in case the agent exits the hypercube
+    state soft bounds.
+
+    ## Starting States
+
+    The initial state is randomly uniformly selected within the polytopic state
+    boundary, so it is guaranteed to be initially safe.
+
+    ## Episode End
+
+    The episode does not have an end, so wrapping it in, e.g., `TimeLimit`, is strongly
+    suggested.
+    """
+
+    ns = 2
+    na = 2
+    A = np.asarray([[1.0, 0.4], [-0.1, 1.0]])
+    B = np.asarray([[1.0, 0.05], [0.5, 1.0]])
+    Q = np.eye(ns)
+    R = 0.1 * np.eye(na)
+    a_bounds = np.asarray([[-0.5, -0.5], [0.5, 0.5]])
+    x_soft_bounds = np.asarray([[-3.0, -3.0], [3.0, 3.0]])
+
+    def __init__(self, constraint_penalty: float = 1e2) -> None:
+        super().__init__()
+        self.observation_space = gym.spaces.Box(-np.inf, np.inf, (self.ns,), np.float64)
+        self.action_space = gym.spaces.Box(*self.a_bounds, (self.na,), np.float64)
+        self.constraint_penalty = constraint_penalty
+        vertices = [
+            self.x_soft_bounds[0],
+            [self.x_soft_bounds[1, 0], self.x_soft_bounds[0, 1]],
+            self.x_soft_bounds[1],
+            [self.x_soft_bounds[0, 0], self.x_soft_bounds[1, 1]],
+        ]
+        self._sampler = ConvexPolytopeUniformSampler(vertices)
+
+        # build also the symbolic dynamics and safety constraints
+        x = cs.MX.sym("x", self.ns)
+        u = cs.MX.sym("u", self.na)
+        x_next = self.A @ x + self.B @ u
+        x_lb, x_ub = self.x_soft_bounds
+        h = cs.veccat(x - x_lb, x_ub - x)
+        self.dynamics = cs.Function("f", [x, u], [x_next], ["x", "u"], ["x_next"])
+        self.safety_constraints = cs.Function("h", [x], [h], ["x"], ["h"])
+
+    def reset(
+        self, *, seed: int | None = None, options: dict[str, Any] | None = None
+    ) -> tuple[ObsType, dict[str, Any]]:
+        super().reset(seed=seed, options=options)
+        self._sampler.seed(self.np_random)
+        sample_on_contour = False if options is None else options.get("contour", False)
+        if sample_on_contour:
+            x = self._sampler.sample_from_surface()
+        else:
+            for _ in range(1000):
+                x = x = self._sampler.sample_from_interior()
+                if np.linalg.norm(x) >= 2.5:
+                    break
+        self.x = x
+        return x, {}
+
+    def step(
+        self, action: npt.ArrayLike
+    ) -> tuple[ObsType, float, bool, bool, dict[str, Any]]:
+        x = self.x
+        u = np.asarray(action).reshape(self.na)
+        assert self.action_space.contains(u), f"invalid action {u}"
+        x_new = np.dot(self.A, x) + np.dot(self.B, u)
+        assert self.observation_space.contains(x_new), f"invalid new state {x_new}"
+        self.x = x_new
+        return x_new, self._compute_cost(x, u), False, False, {}
+
+    def _compute_cost(self, x: ObsType, u: ActType) -> float:
+        lb_violation = np.maximum(0, self.x_soft_bounds[0] - x).sum()
+        ub_violation = np.maximum(0, x - self.x_soft_bounds[1]).sum()
+        return (
+            np.dot(np.dot(self.Q, x), x)
+            + np.dot(np.dot(self.R, u), u)
+            + self.constraint_penalty * (lb_violation + ub_violation)
+        )
