ad1d36c26765ff263af90ab4ce7d4e3ca91d0c64
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-10-28 15:58:48 +0100

    added QPOASES options

--- lti_example/controllers/options.py
+++ lti_example/controllers/options.py
@@ -9,5 +9,11 @@ OPTS = {
         "calc_lam_p": False,
         "calc_lam_x": False,
         "fatrop": {"max_iter": 500, "print_level": 0},
-    }
+    },
+    "qpoases": {
+        "error_on_fail": False,
+        "expand": True,
+        "print_time": False,
+        "printLevel": "none",
+    },
 }
