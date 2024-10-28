96d0a8fad9be6810b6276e5af0c49fe861316610
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-10-28 18:10:56 +0100

    re-implemented DCLF-DCBF with csnlp

--- lti_example/controllers/dclf_dcbf.py
+++ lti_example/controllers/dclf_dcbf.py
@@ -5,16 +5,18 @@ import casadi as cs
 import numpy as np
 import numpy.typing as npt
 from controllers.options import OPTS
+from csnlp import Nlp
 from env import ConstrainedLtiEnv as Env
 from mpcrl.util.control import dcbf, dlqr
 from mpcrl.util.math import clip
 
+nlp = Nlp("MX")
 env = Env()
+a_min = Env.a_bound
 ns, na = env.ns, env.na
-x = cs.MX.sym("x", ns)
-u = cs.MX.sym("u", na)
-delta = cs.MX.sym("delta")
-primals = cs.vertcat(u, delta)
+x = nlp.parameter("x", (ns,))
+u, _, _ = nlp.variable("u", (na,))
+delta, _, _ = nlp.variable("delta", lb=0)
 x_next = env.dynamics(x, u)
 alpha_dclf = 0.5
 alpha_dcbf = 0.5
@@ -25,43 +27,37 @@ _, P = dlqr(env.A, env.B, env.Q, env.R)
 lyapunov = lambda x_: cs.bilin(P, x_)
 V = lyapunov(x)
 V_next = lyapunov(x_next)
-dclf_cnstr = V_next - (1 - alpha_dclf) * V - delta  # <= 0
+nlp.constraint("dclf", V_next - (1 - alpha_dclf) * V, "<=", delta)
 h = env.safety_constraints
 dcbf_cnstr = dcbf(h, x, u, env.dynamics, [lambda y: alpha_dcbf * y])  # >= 0
+nlp.constraint("dcbf", dcbf_cnstr, ">=", 0)
+nlp.minimize(penalty_u * cs.sumsqr(u) + penalty_delta * delta)
 
 # it is a QCQP problem due to the CLF constraint, so we have to solve it nonlinearly
 # as casadi does not support SOCP yet
-qcqp = {
-    "x": primals,
-    "p": x,
-    "f": penalty_u * cs.sumsqr(u) + penalty_delta * delta,
-    "g": cs.vertcat(-dclf_cnstr, dcbf_cnstr),
-}
-solver = cs.nlpsol("solver_dclf_dcbf_qcqp", "fatrop", qcqp, OPTS["fatrop"])
-lbx = np.append(np.full(env.na, -np.inf), 0)  # don't bound the action, but clip it
-ubx = np.full(env.na + 1, np.inf)
-res = solver(p=x, lbx=lbx, ubx=ubx, lbg=0, ubg=np.inf)
-u_dclf_dcbf = clip(res["x"][: env.na], -env.a_bound, env.a_bound)
-controller = cs.Function("dclf_dcbf_qcqp", [x], [u_dclf_dcbf], ["x"], ["u_opt_clip"])
+nlp.init_solver(OPTS["fatrop"], "fatrop", "nlp")
 
-for key, var in locals().copy().items():
-    if key not in ("na", "controller"):
-        del var
+# convert to function
+primals = nlp.x
+u_dclf_dcbf = clip(u[:, 0], -env.a_bound, env.a_bound)
+func = nlp.to_function("dclf_dcbf_qcqp", [x, primals], [u_dclf_dcbf, primals])
 
 
-def get_controller(
+def get_dclf_dcbf_controller(
     *_: Any, **__: Any
 ) -> Callable[[npt.NDArray[np.floating]], npt.NDArray[np.floating]]:
-    """Returns the discrete-time Control Lyapunov Function and Control Barrier Function
-    controller with action saturation.
+    """Returns the discrete-time LQR controller with action saturation.
 
     Returns
     -------
     callable from array-like to array-like
         A controller that maps the current state to the desired action.
     """
+    last_sol = 0.0
 
     def _f(x):
-        return controller(x).full().reshape(na)
+        nonlocal last_sol
+        u, last_sol = func(x, last_sol)
+        return u.toarray().reshape(-1)
 
     return _f
