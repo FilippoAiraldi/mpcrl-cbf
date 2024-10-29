598a784d665de0938a05e1b33fe7a7fc39fa7b3c
Filippo Airaldi <filippoairaldi@gmail.com>
2024-10-29 17:23:56 +0100

    better value func scripts

---
+++ lti_example/value_func/value_func.m
@@ -0,0 +1,62 @@
+%% Solve the explicit MPC problem
+clearvars, clc, close all
+
+% define LTI system
+A = [1, 0.8; -0.1, 1];
+B = [1, 0.05; 0.5, 1];
+model = LTISystem('A', A, 'B', B);
+
+% add penalties
+Q = eye(model.nx);
+R = 0.1 .* eye(model.nu);
+model.x.penalty = QuadFunction(Q);
+model.u.penalty = QuadFunction(R);
+
+% add constraints
+u_bnd = 0.5;
+model.u.min = [-u_bnd; -u_bnd];
+model.u.max = [u_bnd; u_bnd];
+x_bnd = 3;
+model.x.min = [-x_bnd; -x_bnd];
+model.x.max = [x_bnd; x_bnd];
+
+% create MPC problem
+N = 20;
+explicit_mpc = MPCController(model, N).toExplicit();
+% explicit_mpc.partition.plot()
+% explicit_mpc.cost.fplot()
+% xlim([-x_bnd, x_bnd])
+% ylim([-x_bnd, x_bnd])
+% zlim([-5, 80])
+
+
+%% Get the vertices of the (bounded) feasible set
+vertices = explicit_mpc.partition.convexHull.V;
+disp(vertices)
+
+
+%% Extract the optimal value function and policy over the state space
+n_elements = 5;
+ls = linspace(-x_bnd, x_bnd, n_elements);
+[X1, X2] = meshgrid(ls, ls);
+V = zeros(n_elements, n_elements);
+U = zeros(size(B, 2), n_elements, n_elements);
+fprintf("Progress: %3d%%\n", 0);
+for i = 1:n_elements
+    fprintf("\b\b\b\b%3.0f%%", 100 * i / n_elements);
+    for j = 1:n_elements
+        x = [X1(i, j); X2(i, j)];
+        [u,~,openloop] = explicit_mpc.evaluate(x);
+        V(i, j) = openloop.cost;
+        U(:, i, j) = u;
+    end
+end
+
+
+%% Plot and save to disk
+% surf(X1, X2, V, 'EdgeColor','none');
+% title('Value Function')
+% xlabel("x_1")
+% ylabel("x_2")
+% zlabel("V(x)")
+% save(sprintf("value_func_data_%i.mat", N), "X1", "X2", "V")
--- lti_example/value_func/value_func.py
+++ lti_example/value_func/value_func.py
@@ -5,46 +5,21 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(this_dir))
 from itertools import product
 
-import casadi as cs
 import matplotlib.pyplot as plt
 import numpy as np
-from csnlp import Nlp
-from csnlp.wrappers import Mpc
 from env import ConstrainedLtiEnv as Env
 from scipy.io import savemat
 from tqdm import tqdm
+from lti_example.controllers.mpc import create_mpc
 
-# define the parameters
+# get the MPC controller
 N = 20  # approximately an horizon of 10 should be enough, but increase it a bit
-A = Env.A
-B = Env.B
-Q = Env.Q
-R = Env.R
-ns, na = B.shape
-a_bnd = Env.a_bound
-x_bnd = Env.x_soft_bound
-
-# create the MPC problem
-mpc = Mpc(Nlp("MX"), prediction_horizon=N, shooting="single")
-mpc.state("x", ns)
-u, _ = mpc.action("u", na, lb=-a_bnd, ub=a_bnd)
-mpc.set_linear_dynamics(A, B)
-x = mpc.states["x"]
-mpc.constraint("lbx", x, ">=", -x_bnd)
-mpc.constraint("ubx", x, "<=", x_bnd)
-J = sum(cs.bilin(Q, x[:, i]) + cs.bilin(R, u[:, i]) for i in range(N))
-mpc.minimize(J)
-opts = {
-    "error_on_fail": False,
-    "expand": True,
-    "print_time": False,
-    "printLevel": "none",
-}
-mpc.init_solver(opts, "qpoases")
+soft = False  # whether to impose hard or soft constraints on the states
+mpc = create_mpc(N, soft)
 
 # compute the value function
 n_elm = 100
-grid = np.linspace(-x_bnd, x_bnd, n_elm)
+grid = np.linspace(-Env.x_soft_bound, Env.x_soft_bound, n_elm)
 X = np.asarray(np.meshgrid(grid, grid, indexing="ij"))
 V = np.zeros((n_elm, n_elm))
 for i, j in tqdm(product(range(n_elm), range(n_elm)), total=n_elm * n_elm):
@@ -53,11 +28,17 @@ for i, j in tqdm(product(range(n_elm), range(n_elm)), total=n_elm * n_elm):
 
 # save and plot
 savemat(os.path.join(this_dir, "value_func.mat"), {"grid": grid, "V": V})
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 V_ = V[np.isfinite(V)]
-V[V == np.inf] = V_.max()
-ax.plot_surface(X[0], X[1], V, vmin=V_.min(), vmax=V_.max(), cmap="RdBu_r")
-ax.set_xlabel("$x_1$")
-ax.set_ylabel("$x_2$")
-ax.set_zlabel("$V(x)$")
+Vmin, Vmax = V_.min(), V_.max()
+fig = plt.figure(figsize=(10, 5), constrained_layout=True)
+ax1 = fig.add_subplot(1, 2, 1)
+CS = ax1.contour(X[0], X[1], V, vmin=Vmin, vmax=Vmax, cmap="RdBu_r")
+ax1.clabel(CS, inline=True, fontsize=10)
+ax1.set_xlabel("$x_1$")
+ax1.set_ylabel("$x_2$")
+ax2 = fig.add_subplot(1, 2, 2, projection="3d")
+ax2.plot_surface(X[0], X[1], V, vmin=Vmin, vmax=Vmax, cmap="RdBu_r")
+ax2.set_xlabel("$x_1$")
+ax2.set_ylabel("$x_2$")
+ax2.set_zlabel("$V(x)$")
 plt.show()
