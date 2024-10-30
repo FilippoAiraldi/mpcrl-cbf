650d8796cb1e2e5e58ee057e2bd1fa43993e1fc0
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-10-30 17:49:37 +0100

    removed ipopt options; shrunk starting set in env

--- lti_example/controllers/options.py
+++ lti_example/controllers/options.py
@@ -1,15 +1,6 @@
 """File containing options for various optimization solvers."""
 
 OPTS = {
-    "ipopt": {
-        "error_on_fail": False,
-        "expand": True,
-        "print_time": False,
-        "bound_consistency": True,
-        "calc_lam_p": False,
-        "calc_lam_x": True,
-        "ipopt": {"max_iter": 500, "print_level": 0, "sb": "yes"},
-    },
     "fatrop": {
         "error_on_fail": False,
         "expand": True,
