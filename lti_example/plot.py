4f0e47c7b7fc680aed9a38f046fc63ed36e62441
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-11-11 20:56:26 +0100

    improved plotting for training

--- lti_example/plot.py
+++ lti_example/plot.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import sys
 from collections.abc import Collection
 from typing import Any
 
@@ -6,11 +8,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 from csnlp.util.io import load
-from env import ConstrainedLtiEnv as Env
 from matplotlib.gridspec import GridSpec
 from matplotlib.patches import Rectangle
 
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from env import ConstrainedLtiEnv as Env
+
+from util.visualization import plot_population, plot_single_violin
+
 plt.style.use("seaborn-v0_8-pastel")
+plt.rcParams["lines.linewidth"] = 0.75
+plt.rcParams["lines.markersize"] = 6
 
 
 def load_single_file(
@@ -34,64 +43,46 @@ def load_single_file(
     return args, data
 
 
-def plot_states_and_actions_and_return(
-    data: Collection[dict[str, npt.NDArray[np.floating]]],
-    names: Collection[str] | None = None,
+def plot_states_and_actions(
+    data: Collection[dict[str, npt.NDArray[np.floating]]], *_: Any, **__: Any
 ) -> None:
-    """Plots the state trajectories, actions, and returns of the simulations.
+    """Plots the state trajectories and actions of the simulations. This plot does not
+    distinguish between different agents as it plots them all together.
 
     Parameters
     ----------
     data : collection of dictionaries (str, arrays)
-        The dictionaries from different simulations, each containing the keys `"cost"`,
-        `"actions"`, `"states"`, and `"sol_times"`.
-    names : collection of str, optional
-        The names of the simulations to use in the plot.
+        The dictionaries from different simulations, each containing the keys
+        `"actions"` and `"states"`.
     """
     fig = plt.figure(constrained_layout=True)
-    gs = GridSpec(3, 2, fig)
-    lw = 1.0
+    gs = GridSpec(2, 2, fig)
     ax1 = fig.add_subplot(gs[:, 0])
     ax2 = fig.add_subplot(gs[0, 1])
     ax3 = fig.add_subplot(gs[1, 1], sharex=ax2)
-    ax4 = fig.add_subplot(gs[2, 1])
 
     x_max = Env.x_soft_bound
     ax1.add_patch(Rectangle((-x_max, -x_max), 2 * x_max, 2 * x_max, fill=False))
 
     for i, datum in enumerate(data):
-        returns = datum["cost"]  # n_ctrl x n_eval
-        actions = datum["actions"]  # n_ctrl x n_eval x timesteps x na
-        states = datum["states"]  # n_ctrl x n_eval x timesteps + 1 x ns
+        returns = datum["cost"]  # n_agents x n_ep
+        actions = datum["actions"]  # n_agents x n_ep x timesteps x na
+        states = datum["states"]  # n_agents x n_ep x timesteps + 1 x ns
         c = f"C{i}"
         timesteps = actions.shape[2]
         time = np.arange(timesteps)
 
-        # flatten the first two axes as we do not distinguish between different
+        # flatten the first two axes as we do not distinguish between different agents
         returns = returns.reshape(-1)
         actions = actions.reshape(-1, timesteps, Env.na)
         states = states.reshape(-1, timesteps + 1, Env.ns)
 
         for state_traj, action_traj in zip(states, actions):
-            ax1.plot(*state_traj.T, c, lw=lw)
+            ax1.plot(*state_traj.T, c)
             violating = (np.abs(state_traj) > x_max + 1e-3).any(1)
-            ax1.plot(*state_traj[violating].T, "r", ls="none", marker="x", ms=6)
-            ax2.step(time, action_traj[:, 0], c, lw=lw, where="post")
-            ax3.step(time, action_traj[:, 1], c, lw=lw, where="post")
-
-        print(f"cost: {np.mean(returns)} ± {np.std(returns)} | {np.median(returns)}")
-        st = datum["sol_times"]
-        print(f"sol. time: {np.mean(st):e} ± {np.std(st):e} | {np.median(st):e}")
-
-        VL = ax4.violinplot(returns, [i], showmedians=True, showextrema=False)
-        perpline = VL["cmedians"].get_paths()[0]
-        path = VL["bodies"][0].get_paths()[0].vertices
-        median = perpline.vertices[0, 1]
-        closest = np.abs(path[:, 1] - median).argmin()
-        width = np.abs(path[closest, 0]) - i
-        perpline.vertices[:, 0] = [i - width, i + width]
-        pos = i + np.random.uniform(-0.01, 0.01, size=len(returns))
-        ax4.scatter(pos, returns, s=10, facecolor="none", edgecolors=c)
+            ax1.plot(*state_traj[violating].T, "r", ls="none", marker="x")
+            ax2.step(time, action_traj[:, 0], c, where="post")
+            ax3.step(time, action_traj[:, 1], c, where="post")
 
     ax1.set_xlabel("$x_1$")
     ax1.set_ylabel("$x_2$")
@@ -99,10 +90,157 @@ def plot_states_and_actions_and_return(
     ax2.set_ylabel("$u_1$")
     ax3.set_ylabel("$u_2$")
     ax3.set_xlabel("$k$")
-    ax4.set_xticks(range(len(data)))
+
+
+def plot_returns(
+    data: Collection[dict[str, npt.NDArray[np.floating]]],
+    names: Collection[str] | None = None,
+) -> None:
+    """Plots the returns of the simulations.
+
+    Parameters
+    ----------
+    data : collection of dictionaries (str, arrays)
+        The dictionaries from different simulations, each containing the key `"cost"`.
+    names : collection of str, optional
+        The names of the simulations to use in the plot.
+    """
+    _, (ax1, ax2) = plt.subplots(2, 1, constrained_layout=True)
+
+    for i, datum in enumerate(data):
+        c = f"C{i}"
+        returns = datum["cost"]  # n_agents x n_ep
+        episodes = np.arange(returns.shape[1])
+        # print(f"cost: {np.mean(returns)} ± {np.std(returns)} | {np.median(returns)}")
+        # in the first axis, flatten the first two axes as we do not distinguish between
+        # different agents
+        returns_flat = returns.reshape(-1)
+        plot_single_violin(
+            ax1,
+            i,
+            returns_flat,
+            showextrema=False,
+            quantiles=[0.25, 0.5, 0.75],
+            color=c,
+        )
+        plot_population(ax2, episodes, returns, axis=0, color=c)
+
+    ax1.set_xticks(range(len(data)))
     if names is not None:
-        ax4.set_xticklabels(names)
-    ax4.set_ylabel("Cost")
+        ax1.set_xticklabels(names)
+    ax1.set_ylabel("Return")
+    ax2.set_xlabel("Episode")
+    ax2.set_ylabel("Return")
+
+
+def plot_solver_times(
+    data: Collection[dict[str, npt.NDArray[np.floating]]],
+    names: Collection[str] | None = None,
+) -> None:
+    """Plots the solver times of the simulations. For agents with a solver for the
+    state and action value functions, their computation times are plotted separately.
+
+    Parameters
+    ----------
+    data : collection of dictionaries (str, arrays)
+        The dictionaries from different simulations, each containing the key
+        `"sol_times"`.
+    names : collection of str, optional
+        The names of the simulations to use in the plot.
+    """
+    _, ax = plt.subplots(1, 1, constrained_layout=True)
+
+    for i, datum in enumerate(data):
+        c = f"C{i}"
+        sol_times = datum["sol_times"]  # n_agents x n_ep x timestep (x 2, optional)
+        kw = {"showextrema": False, "quantiles": [0.25, 0.5, 0.75], "color": c}
+
+        # flatten the first three axes as we do not distinguish between different
+        # agents, episodes or time steps
+        sol_times_flat = sol_times.reshape(-1, *sol_times.shape[3:])
+        # print(f"sol. time: {np.mean(st):e} ± {np.std(st):e} | {np.median(st):e}")
+        if sol_times_flat.ndim == 1:
+            plot_single_violin(ax, i, sol_times_flat, **kw)
+        else:
+            assert sol_times_flat.ndim == 2, "Unexpected shape of `sol_times_flat`"
+            sol_times_V, sol_times_Q = sol_times_flat.T
+            for side, times in (("low", sol_times_V), ("high", sol_times_Q)):
+                plot_single_violin(ax, i, times, side=side, **kw)
+
+    ax.set_xticks(range(len(data)))
+    if names is not None:
+        ax.set_xticklabels(names)
+    ax.set_ylabel("Solver time [s]")
+    ax.set_yscale("log")
+
+
+def plot_training(
+    data: Collection[dict[str, npt.NDArray[np.floating]]], *_: Any, **__: Any
+) -> None:
+    """Plots the training results of the simulations. This plot does not show anything
+    if the data does not contain training results.
+
+    Parameters
+    ----------
+    data : collection of dictionaries (str, arrays)
+        The dictionaries from different simulations, each potentially containing the
+        keys `"updates_history"` and `"td_errors"`.
+    """
+    param_names = set()
+    for datum in data:
+        if "updates_history" in datum:
+            param_names.update(datum["updates_history"].keys())
+    any_td_errors = any("td_errors" in d for d in data)
+    n = len(param_names)
+    if n == 0 and not any_td_errors:
+        return
+
+    fig = plt.figure(constrained_layout=True)
+    ncols = int(np.round(np.sqrt(n)))
+    nrows = int(np.ceil(n / ncols))
+    gs = GridSpec(nrows + int(any_td_errors), ncols, fig)
+    if any_td_errors:
+        ax_td = fig.add_subplot(gs[0, :])
+        offset = 1
+    else:
+        offset = 0
+    param_names = sorted(param_names)
+    start = nrows * ncols - n
+    ax_par_first = fig.add_subplot(gs[start // ncols + offset, start % ncols])
+    ax_pars = {param_names[0]: ax_par_first}
+    for i, name in enumerate(param_names[1:], start=start + 1):
+        ax_pars[name] = fig.add_subplot(
+            gs[i // ncols + offset, i % ncols], sharex=ax_par_first
+        )
+
+    for i, datum in enumerate(data):
+        c = f"C{i}"
+
+        if "td_errors" in datum:
+            td_errors = datum["td_errors"]  # n_agents x n_ep x timestep
+            td_errors = np.abs(td_errors).sum(2)
+            episodes = np.arange(td_errors.shape[1])
+            plot_population(ax_td, episodes, td_errors, axis=0, color=c)
+
+        if "updates_history" in datum:
+            for name, param in datum["updates_history"].items():
+                updates = np.arange(param.shape[1])
+                param = param.reshape(*param.shape[:2], -1)  # n_agents x n_up x ...
+                ax = ax_pars[name]
+                n = param.shape[2]
+                alpha = 0.25 if n == 1 else 0
+                for idx in range(param.shape[2]):
+                    plot_population(
+                        ax, updates, param[..., idx], axis=0, color=c, alpha=alpha
+                    )
+
+    if any_td_errors:
+        ax_td.set_xlabel("Episode")
+        ax_td.set_ylabel(r"$\sum{|\delta|}$")
+    for name, ax in ax_pars.items():
+        ax.set_xlabel("Episode")
+        ax.set_ylabel(name)
+        ax._label_outer_xaxis(skip_non_rectangular_axes=False)
 
 
 if __name__ == "__main__":
@@ -128,5 +266,8 @@ if __name__ == "__main__":
         data.append(datum)
         print(filename.upper(), f"Args: {sim_args}\n", sep="\n")
 
-    plot_states_and_actions_and_return(data, unique_names)
+    plot_states_and_actions(data, unique_names)
+    plot_returns(data, unique_names)
+    plot_solver_times(data, unique_names)
+    plot_training(data, unique_names)
     plt.show()
---
+++ util/visualization.py
@@ -0,0 +1,159 @@
+from typing import Any, Literal
+
+import numpy as np
+import numpy.typing as npt
+from matplotlib.axes import Axes
+from scipy.interpolate import CubicSpline
+
+
+def plot_population(
+    ax: Axes,
+    x: npt.ArrayLike,
+    y: npt.ArrayLike,
+    axis: int | tuple[int, ...],
+    use_quartiles: bool = False,
+    log: bool = False,
+    marker: str | None = None,
+    ls: str | None = None,
+    color: str | None = None,
+    alpha: float = 0.25,
+) -> None:
+    """Plots a quantity repeated across a population. The mean/median is plotted as a
+    line, and the area between the mean ± std or median ± quartiles is filled.
+
+    Parameters
+    ----------
+    ax : Axes
+        The axis to plot on.
+    x : array-like of shape `(n,)`
+        The x-axis values.
+    y : array-like of shape `(..., n, ...)`
+        The y-axis values. This contains the values for each member of the population.
+        It can have any shape, as long as it contains the `n` dimension. See `axis` in
+        order to reduce the population to a single value.
+    axis : int | tuple[int, int], optional
+        Axis along which to compute the mean/median and std/quartiles. After reduction,
+        the shape must be `(n,)` in order to be plotted.
+    use_quartiles : bool, optional
+        If `True`, the median and quartiles are used instead of the mean and std, by
+        default `False`.
+    log : bool, optional
+        If `True`, the y-axis is plotted in log scale, by default `False`.
+    marker : str, optional
+        The marker to use in the plot, by default `None`.
+    ls : str, optional
+        The line style to use in the plot, by default `None
+    color : str, optional
+        The color to use in the plot, by default `None`.
+    alpha : float, optional
+        The transparency of the filled area, by default `0.25`.
+    """
+    if use_quartiles:
+        lower, middle, upper = np.nanquantile(y, [0.25, 0.5, 0.75], axis)
+    else:
+        middle = np.nanmean(y, axis)
+        std = np.nanstd(y, axis)
+        lower = middle - std
+        upper = middle + std
+    method = ax.semilogy if log else ax.plot
+    c = method(x, middle, label=None, marker=marker, ls=ls, color=color)[0].get_color()
+    if alpha > 0:
+        ax.fill_between(x, lower, upper, alpha=alpha, color=c, label=None)
+
+
+def plot_single_violin(
+    ax: Axes,
+    position: float,
+    data: npt.ArrayLike,
+    scatter: bool = True,
+    color: str | None = None,
+    alpha: float = 0.25,
+    vert: bool = True,
+    side: Literal["both", "low", "high"] = "both",
+    gap: float = 0.0,
+    **other_violin_kwargs: Any,
+):
+    """Plots a single violin plot.
+
+    Parameters
+    ----------
+    ax : Axes
+        The axis to plot on.
+    position : float
+        The position of the violin plot on the x-axis, if `vert`, or on the y-axis.
+    data : 1d array-like
+        The data to plot.
+    color : str, optional
+        The color of the scattered and violin data, by default `None`.
+    alpha : float, optional
+        The transparency of the violin plot, by default `0.25`.
+    vert : bool, optional
+        If `True`, the violin plot is vertical, by default `True`.
+    side : {"both", "low", "high"}, optional
+        The side of the violin plot to plot, by default `"both"`.
+    gap : float, optional
+        Positive gap between sided violin plots, by default `0.0`.
+    violin_kwargs : dict
+        Other keywords arguments to pass to the violin plot.
+    """
+    data = np.asarray(data)
+    if side == "low":
+        position -= gap
+    elif side == "high":
+        position += gap
+    violin_data = ax.violinplot(
+        data, [position], vert, side=side, **other_violin_kwargs
+    )
+
+    # plot scattered data
+    if scatter:
+        if side == "low":
+            lb, ub = -0.01, 0.0
+        elif side == "high":
+            lb, ub = 0.0, 0.01
+        else:
+            lb, ub = -0.01, 0.01
+        positions = position + np.random.uniform(lb, ub, size=data.size)
+        ax.scatter(positions, data, s=10, facecolor="none", edgecolors=color)
+
+    # the rest of the method adjustes the length of the violin lines to match the
+    # distribution and adjust the color
+    bodies = violin_data["bodies"]
+    assert len(bodies) == 1, "only one violin plot is supported! Something went wrong."
+    body = bodies[0]
+    body.set_facecolor(color)
+    body.set_alpha(alpha)
+
+    violin_vertices = body.get_paths()[0].vertices
+    N = (violin_vertices.shape[0] - 1) // 2
+    if side in ("low", "both"):
+        X = violin_vertices[1:N, 0]
+        Y = violin_vertices[1:N, 1]
+    else:
+        X = violin_vertices[-2 : -N - 1 : -1, 0]
+        Y = violin_vertices[-2 : -N - 1 : -1, 1]
+    if vert:
+        X, Y = Y, X
+    interp = CubicSpline(X, Y)
+
+    int_vert = int(vert)
+    int_not_vert = int(not vert)
+
+    for k in ("cmeans", "cmedians", "cquantiles"):
+        if k not in violin_data:
+            continue
+
+        linecollection = violin_data[k]
+        if color:
+            linecollection.set_color(color)
+
+        for line in linecollection.get_paths():
+            value = line.vertices[0, int_vert]
+            length = abs(interp(value)) - position
+            print(side, length)
+            if side == "low":
+                line.vertices[0, int_not_vert] = position - length
+            elif side == "high":
+                line.vertices[1, int_not_vert] = position + length
+            else:
+                line.vertices[:, int_not_vert] = [position - length, position + length]
