3a7fcea53e931135f39044c33df3d5ab7feb6e94
Filippo Airaldi <f.airaldi@tudelft.nl>
2024-11-20 13:48:00 +0100

    modified env reward to depend on DCBF constraint

--- lti_example/env.py
+++ lti_example/env.py
@@ -4,6 +4,8 @@ import casadi as cs
 import gymnasium as gym
 import numpy as np
 import numpy.typing as npt
+from controllers.config import DCBF_GAMMA
+from mpcrl.util.control import dcbf
 from mpcrl.util.geometry import ConvexPolytopeUniformSampler
 
 from util.loose_box import LooseBox
@@ -83,13 +85,19 @@ class ConstrainedLtiEnv(gym.Env[ObsType, ActType]):
         self.action_space = LooseBox(-a_max, a_max, (self.na,), np.float64)
         self._sampler = ConvexPolytopeUniformSampler(MAX_INV_SET_V)
         self._max_timesteps = max_timesteps
+
         # build also the symbolic dynamics and safety constraints
         x = cs.MX.sym("x", self.ns)
         u = cs.MX.sym("u", self.na)
         x_next = self.A @ x + self.B @ u
+        dynamics = cs.Function("f", [x, u], [x_next], ["x", "u"], ["x_next"])
         h = cs.veccat(x + x_max, x_max - x)  # >= 0
-        self.dynamics = cs.Function("f", [x, u], [x_next], ["x", "u"], ["x_next"])
-        self.safety_constraints = cs.Function("h", [x], [h], ["x"], ["h"])
+        safety_constraints = cs.Function("h", [x], [h], ["x"], ["h"])
+        dcbf_ = dcbf(safety_constraints, x, u, dynamics, [lambda y: DCBF_GAMMA * y])
+        dcbf_constraints = cs.Function("h", [x, u], [dcbf_], ["x", "u"], ["dcbf"])
+        self.dynamics = dynamics
+        self.safety_constraints = safety_constraints
+        self.dcbf_constraints = dcbf_constraints
 
     def reset(
         self, *, seed: int | None = None, options: dict[str, Any] | None = None
@@ -128,12 +136,11 @@ class ConstrainedLtiEnv(gym.Env[ObsType, ActType]):
         return x_new, self._compute_cost(x, u), False, truncated, {}
 
     def _compute_cost(self, x: ObsType, u: ActType) -> float:
-        lb_violation = np.maximum(0, -self.x_soft_bound - x).sum()
-        ub_violation = np.maximum(0, x - self.x_soft_bound).sum()
+        violations = np.maximum(0.0, -self.dcbf_constraints(x, u)).sum()
         return (
             np.dot(np.dot(self.Q, x), x)
             + np.dot(np.dot(self.R, u), u)
-            + self.constraint_penalty * (lb_violation + ub_violation)
+            + self.constraint_penalty * violations
         )
 
     def sample_disturbance_profiles(
