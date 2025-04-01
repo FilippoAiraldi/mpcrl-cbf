b4a4443d9fb6fd0208a16451a17a04fe6b8a42d1
Filippo Airaldi <f.airaldi@tudelft.nl>
2025-04-01 02:06:32 +0200

    moving to another branch

--- util/nn.py
+++ util/nn.py
@@ -1,147 +1,13 @@
-from collections.abc import Generator, Sequence
-from typing import Literal, TypeVar
-
 import casadi as cs
-import numpy as np
-from csnn import Linear, Module, ReLU
-from csnn.convex import PsdNN, PwqNN
-from csnn.convex.psd import _reshape_mat
-from numpy.typing import ArrayLike, NDArray
-
-SymType = TypeVar("SymType", cs.SX, cs.MX)
+from csnn.convex import PwqNN
 
 
-class DynTanh(Module[SymType]):
-    """Dynamic tanh normalization layer from https://arxiv.org/abs/2503.10622.
-
-    Parameters
-    ----------
-    num_features : int
-        Number of input features.
-    """
-
-    def __init__(self, num_features: int) -> None:
-        super().__init__()
-        self.num_features = num_features
-        self.factor = self.sym_type.sym("factor", 1, num_features)
-        self.scale = self.sym_type.sym("scale", 1, num_features)
-        self.offset = self.sym_type.sym("offset", 1, num_features)
-
-    def _custom_init(
-        self, *_, **__
-    ) -> Generator[tuple[str, NDArray[np.floating]], None, None]:
-        """Custom initialization of the parameters."""
-        yield "factor", np.full(self.factor.shape, 0.5)
-        yield "scale", np.ones(self.scale.shape)
-        yield "offset", np.zeros(self.offset.shape)
-
-    def forward(self, input: SymType) -> SymType:
-        """Computes the normalized output.
-
-        Parameters
-        ----------
-        input : SymType
-            The input tensor of shape `(batch_size, num_features)`.
-
-        Returns
-        -------
-        SymType
-            The output tensor of shape `(batch_size, num_features)`.
-        """
-        y = cs.tanh(self.factor * input)
-        return self.offset + self.scale * y
-
-    def extra_repr(self) -> str:
-        return f"{self.num_features}"
-
-
-class QuadrotorNN(PsdNN):
-    """Network for the `QuadrotorEnv` that combines positive semidefinite terminal
-    cost approximation and a class Kappa function approximation (with shared
-    parameters).
-
-    Parameters
-    ----------
-    in_features : int
-        Number of input features.
-    hidden_features : sequence of ints
-        Number of features in each hidden linear layer.
-    out_size : int
-        Size of the quadratic form elements (i.e., the side length of the PSD matrix).
-    out_shape : {"flat", "triu", "tril"}
-        Shape of the output PSD matrix. If "flat", the output is not reshaped in any
-        matrix. If "triu" or "tril", the output is reshaped as an upper or lower
-        triangular, but does not support batched inputs.
-    act : type of activation function, optional
-        Class of the activation function. By default, `ReLU` is used.
-    eps : array-like, optional
-        Value to add to the PSD matrix, e.g., to ensure it is positive definite. Should
-        be broadcastable to the shape `(out_size, out_size)`. By default, an identity
-        matrix with `1e-4` is used. Only used in the `quadform` method.
-
-    Raises
-    ------
-    ValueError
-        Raises if the number of hidden layers is less than 1.
-    """
-
-    def __init__(
-        self,
-        in_features: int,
-        hidden_features: Sequence[int],
-        out_size: int,
-        out_shape: Literal["flat", "triu", "tril"],
-        act: type[Module] = ReLU,
-        eps: ArrayLike | None = None,
-    ) -> None:
-        super().__init__(in_features, hidden_features, out_size, out_shape, act, eps)
-        self.normalization = DynTanh(in_features)
-        self.cbf_head = Linear(hidden_features[-1], 1)
-
-    def _forward(self, input: SymType) -> tuple[SymType, SymType, SymType]:
-        h = self.hidden_layers(self.normalization(input))
-        ref = self.ref_head(h)
-        mat_flat = self.mat_head(h)
-        gamma = (cs.tanh(self.cbf_head(h)) + 1) / 2
-        return mat_flat, ref, gamma
-
-    def forward(self, input: SymType) -> tuple[SymType, SymType, SymType]:
-        mat, ref, gamma = self._forward(input)
-        if self._out_shape != "flat":
-            mat = _reshape_mat(mat, self._eps.size1(), self._out_shape)
-        return mat, ref, gamma
-
-    def terminal_cost_and_kappa(
-        self, x: SymType, context: SymType
-    ) -> tuple[SymType, SymType]:
-        """Computes the terminal cost quadratic form `(x - x_ref)' Q (x - x_ref)`, where
-        `Q` is the predicted the PSD matrix and `x_ref` the reference point, as well as
-        the class Kappa function's linear factor.
-
-        Parameters
-        ----------
-        x : SymType
-            The value at which the quadratic form is evaluated.
-        context : SymType
-            The context passed as input to the neural network for the prediction of the
-            PSD matrix and the reference point.
-
-        Returns
-        -------
-        SymType
-            The value of the quadratic form.
-        """
-        L_flat, ref, gamma = self._forward(context)
-        L = _reshape_mat(L_flat, self._eps.size1(), "tril")
-        return cs.bilin(L @ L.T + self._eps, x - ref), gamma
-
-
-def nn2function(net: QuadrotorNN | PwqNN, prefix: str) -> cs.Function:
+def nn2function(net: PwqNN, prefix: str) -> cs.Function:
     """Converts a neural network model into a CasADi function.
 
     Parameters
     ----------
-    net : QuadrotorNN, or PwqNN
+    net : PwqNN
         The neural network that must be converted to a function.
     prefix : str
         Prefix to add in front of the net's parameters. Used also to name the CasADi
@@ -165,21 +31,8 @@ def nn2function(net: QuadrotorNN | PwqNN, prefix: str) -> cs.Function:
         outputs = [net.forward(x.T)]
         output_names = ["V"]
 
-    elif isinstance(net, QuadrotorNN):
-        in_features = net.hidden_layers[0].in_features
-        out_features = net.ref_head.weight.size1()
-        context = net.sym_type.sym("x", in_features, 1)
-        x = net.sym_type.sym("x", out_features, 1)
-
-        inputs = [x, context]
-        input_names = ["x", "context"]
-        outputs = net.terminal_cost_and_kappa(x.T, context.T)
-        output_names = ["V", "gamma"]
-
     else:
-        raise ValueError(
-            "The neural network model must be an instance of QuadrotorNN or PwqNN."
-        )
+        raise ValueError("The neural network model must be an instance of PwqNN.")
 
     parameters = dict(net.parameters(prefix=prefix, skip_none=True))
     return cs.Function(
